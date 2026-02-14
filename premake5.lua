f77fdf3ea68c4633647497a2e0a9e2471a18c772
Maksymilian Saferyjski <maksymilian.saferyjski01@gmail.com>
2026-02-14 20:37:30 +0100

    initial commit

---
+++ premake5.lua
@@ -0,0 +1,17 @@
+workspace "http-server"
+   configurations { "Debug", "Release" }
+   location "build"
+
+project "http-server"
+   kind "ConsoleApp"
+   language "C"
+   files { "src/**.h", "src/**.c" }
+
+   filter { "configurations:Debug" }
+      buildoptions {"-Wall", "-pedantic"}
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter { "configurations:Release" }
+      defines { "NDEBUG" }
+      optimize "On"
