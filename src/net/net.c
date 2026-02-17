4298915c9d3db79368465579f9363b58532f1a89
Maksymilian Saferyjski <maksymilian.saferyjski01@gmail.com>
2026-02-17 18:34:13 +0100

    feat(net): abstracted bind, listen, setsockopt into own functions

--- src/net/net.c
+++ src/net/net.c
@@ -9,41 +9,116 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-#define MAX_UNTIL_READ 4000
+i32 bind_socket(socket_t* sock) 
+{
+    if (!sock)
+        return -1;
 
-socket_t* create_socket(const char* addr, short PORT, prot_t prot) {
+    return bind(sock->socket_fd, (struct sockaddr*)&(sock->sockaddr), sizeof(sock->sockaddr));
+}
+
+i32 listen_socket(socket_t* sock, i32 n) {
+    if (!sock || sock->type == NET_UDP) 
+        return -1;
+
+    return listen(sock->socket_fd, n);
+}
+
+socket_t* create_socket(net_family_t family, const char* addr, short port, net_prot_t prot) {
     socket_t* sock = malloc(sizeof(socket_t));
-    i32 socket_fd = socket(AF_INET, prot, 0);
-    if(socket_fd < 0) {
-        perror("net: socket");
+
+    if (!sock) {
+        perror("net: create_socket malloc");
         return NULL;
     }
 
-    i32 opt = 1;
-    if(setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-        perror("net: setsockopt");
-        close(socket_fd);
+    // translate str address to bin presentation
+    if (inet_pton(family, addr, &(sock->sockaddr.sin_addr.s_addr)) <= 0) {
+        free(sock);
+        return NULL;
+    }
+
+    i32 socket_fd = socket(family, prot, 0);
+    if (socket_fd == -1) {
+        free(sock);
         return NULL;
     }
 
     sock->socket_fd = socket_fd;
-    sock->sockaddr.sin_family = AF_INET;
-    sock->sockaddr.sin_port = htons(PORT);
+    sock->sockaddr.sin_family = family;
+    sock->sockaddr.sin_port = htons(port);
+    sock->type = prot;
+    return sock;
+} 
+
+i32 set_socket_opt(socket_t* sock, i32 opt, i32 optval) {
+    if (!sock) 
+        return -1;
     
-    if( inet_pton(AF_INET, addr, &(sock->sockaddr.sin_addr.s_addr)) <= 0) {
-        perror("net: inet_pton");
+    return setsockopt(sock->socket_fd, SOL_SOCKET, opt, &optval, sizeof(optval));
+}
+
+socket_t* create_server_socket(const char* addr, short port, net_prot_t prot) {
+    if (!addr)
+        return NULL;
+
+    socket_t* sock = create_socket(IPV4, addr, port, prot);
+    if (!sock) {
+        perror("net: create_socket");
         return NULL;
     }
 
-    if(bind(socket_fd, (struct sockaddr*)&(sock->sockaddr), sizeof(sock->sockaddr)) < 0) {
+    if (set_socket_opt(sock, SO_REUSEADDR, 1)) {
+        close(sock->socket_fd);
+        free(sock);
+        perror("net: setsockopt");
+        return NULL;
+    }
+ 
+    if (bind_socket(sock) == -1) {
+        close(sock->socket_fd);
+        free(sock);
         perror("net: bind");
         return NULL;
     }
 
-    if(listen(sock->socket_fd, 3) < 0 ) {
-        perror("net: listen");
+    if (listen_socket(sock, 3) == -1 ) {
+        close(sock->socket_fd);
+        free(sock);
+        perror("net: listen_socket");
         return NULL;
     }
 
     return sock;
 } 
+
+socket_t* accept_socket(socket_t* sock) {
+    if (!sock)
+        return NULL;
+
+    socket_t* new_sock = malloc(sizeof(socket_t));
+    socklen_t addr_len = sizeof(new_sock->sockaddr);
+    i32 res_fd = accept(sock->socket_fd, (struct sockaddr*) &(new_sock->sockaddr), &addr_len);
+    if (res_fd == -1) {
+        free(new_sock);
+        perror("net: accept");
+        return NULL;
+    }
+
+    new_sock->socket_fd = res_fd;
+    return new_sock;
+}
+
+ssize_t read_socket(socket_t* sock, void* buf, size_t len) {
+    if (!sock || !buf)
+        return -1;
+
+    return recv(sock->socket_fd, buf, len, 0);
+}
+
+ssize_t write_socket(socket_t* sock, void* buf, size_t len) {
+    if (!sock || !buf) 
+        return -1;
+
+    return send(sock->socket_fd, buf, len, 0);
+}
--- src/net/net.h
+++ src/net/net.h
@@ -1,17 +1,31 @@
 #ifndef NET_H
 #define NET_H
 
+#include "../prelude.h"
+
 #include <netinet/in.h>
 #include <sys/socket.h>
+
+typedef enum {
+    NET_TCP = 1,
+    NET_UDP
+} net_prot_t;
+
+typedef enum {
+    IPV4 = 2,
+    IPV6 = 10,
+} net_family_t;
 
 typedef struct {
     int socket_fd;
     struct sockaddr_in sockaddr;
+    net_prot_t type;
 } socket_t;
 
-typedef enum {
-    NET_TCP = 1,
-    NET_UDP = 2
-} prot_t;
+
+socket_t* create_server_socket(const char* addr, short PORT, net_prot_t prot);
+socket_t* accept_socket(socket_t* sock);
+ssize_t read_socket(socket_t* sock, void* buf, size_t len);
+ssize_t write_socket(socket_t* sock, void* buf, size_t len);
 
 #endif
